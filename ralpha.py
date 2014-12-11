8b149c90908205afe8c83bf7df65dfd51d067fae
foxhead128 <foxhead128@gmail.com>
2014-12-10 21:16:01 -0500

    Added reverse alphabet cipher.

---
+++ ralpha.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# "Reverse alphabet cipher"
+
+import sys, re
+
+table0 = {"a": "z",
+          "b": "y",
+          "c": "x",
+          "d": "w",
+          "e": "v",
+          "f": "u",
+          "g": "t",
+          "h": "s",
+          "i": "r",
+          "j": "q",
+          "k": "p",
+          "l": "o",
+          "m": "n",
+          "n": "m",
+          "o": "l",
+          "p": "k",
+          "q": "j",
+          "r": "i",
+          "s": "h",
+          "t": "g",
+          "u": "f",
+          "v": "e",
+          "w": "d",
+          "x": "c",
+          "y": "b",
+          "z": "a",
+          "A": "Z",
+          "B": "Y",
+          "C": "X",
+          "D": "W",
+          "E": "V",
+          "F": "U",
+          "G": "T",
+          "H": "S",
+          "I": "R",
+          "J": "Q",
+          "K": "P",
+          "L": "O",
+          "M": "N",
+          "N": "M",
+          "O": "L",
+          "P": "K",
+          "Q": "J",
+          "R": "I",
+          "S": "H",
+          "T": "G",
+          "U": "F",
+          "V": "E",
+          "W": "D",
+          "X": "C",
+          "Y": "B",
+          "Z": "A"}
+
+def decode(string, translate=False, html_mode=False):
+    array = []
+    for character in string:
+        if character in table0.keys():
+            array.append(table0[character])
+        else:
+            array.append(character)
+    if translate:
+        if html_mode:
+            array.append("<br>")
+        else:
+            array.append("\n")
+        array.append(string)
+    return "".join(array)
+
+def main(argv=[]):
+    vinfo = tuple(str(x) for x in sys.version_info[:3])
+    print("QrivaN Cipher (Python %s)" % (".".join(vinfo),))
+    print("Enter blank input for options...")
+    x = " ".join(argv)
+    options = {"translation": False, "HTML mode": False}
+    if len(x) == 0:
+        x = input("> ")
+    while 1:
+        if x == "":
+            y = input("Options\n%s\n>>> " % ("\n".join([x[0][0] + ": " + ("Disable " if x[1] else "Enable ") + x[0] for x in options.items()])))
+            changed = False
+            for key in options.keys():
+                if y.lower() == str(key)[0].lower():
+                    options[key] = not options[key]
+                    print("%s %s." % (key.capitalize(), "enabled" if options[key] else "disabled"))
+                    changed = True
+                    break
+            if not changed:
+                print("Nothing changed.")
+        else:
+            print(decode(x, options["translation"], options["HTML mode"]))
+        x = input("> ")
+
+if __name__ == "__main__":
+    main(sys.argv[1::])
